c512d325134743efb987e1d4589f956e2e9fe2d4
jinmu333 <dreamhunter2333@gmail.com>
2023-03-27 22:42:38 +0800

    init

--- main.go
+++ main.go
@@ -1,34 +1,142 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/yalp/jsonpath"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	r := gin.Default()
+var targetURL = "https://api.openai.com"
+var authPrefix = "Bearer "
 
-	r.Any("/*path", func(c *gin.Context) {
-		remoteURL := "https://api.openai.com" + c.Param("path")
-		req, _ := http.NewRequest(c.Request.Method, remoteURL, c.Request.Body)
-		for k, v := range c.Request.Header {
-			req.Header.Set(k, v[0])
+func getProxyUrl() *http.Transport {
+	// Check if HTTP_PROXY environment variable is set
+	if proxyUrl, ok := os.LookupEnv("http_proxy"); ok {
+		fmt.Println("HTTP_PROXY: " + proxyUrl)
+		proxyURL, _ := url.Parse(proxyUrl)
+		transport := &http.Transport{
+			Proxy: http.ProxyURL(proxyURL),
 		}
+		return transport
+	}
+	return nil
+}
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
+func getApiKey() string {
+	// use self API_KEY
+	if apiKey, ok := os.LookupEnv("API_KEY"); ok {
+		fmt.Println("API_KEY: ******")
+		return apiKey
+	}
+	return ""
+}
+
+func proxy(c *gin.Context, transport *http.Transport) (*http.Response, error) {
+	var client *http.Client
+
+	remoteURL := targetURL + c.Param("path")
+	req, _ := http.NewRequest(c.Request.Method, remoteURL, c.Request.Body)
+
+	// 设置请求Header
+	req.Header = c.Request.Header
+
+	// new http Client
+	if transport != nil {
+		client = &http.Client{
+			Transport: transport,
+		}
+	} else {
+		client = &http.Client{}
+	}
+
+	return client.Do(req)
+}
+
+func getTotalTokens(respBody []byte) (int, error) {
+	var data interface{}
+	err := json.Unmarshal(respBody, &data)
+	if err != nil {
+		return 0, err
+	}
+	result, err := jsonpath.Read(data, "$.usage.total_tokens")
+	if err != nil {
+		return 0, err
+	}
+	return int(result.(float64)), nil
+}
+
+func checkSelfApiKey(selfApiKey string) error {
+	// TODO: check selfApiKey and balance
+	return nil
+}
+
+func increaseSelfApiKeyTokens(respBody []byte, selfApiKey string) error {
+	totalTokens, err := getTotalTokens(respBody)
+	if err != nil {
+		return err
+	}
+	// TODO: count totalTokens in db
+	fmt.Println(selfApiKey, totalTokens)
+	return nil
+}
+
+func handler(c *gin.Context, transport *http.Transport, apiKey string) {
+	var selfApiKey string = ""
+	// use self API_KEY
+	if apiKey != "" {
+		authHeader := c.Request.Header.Get("Authorization")
+		if authHeader != "" && strings.HasPrefix(authHeader, authPrefix) {
+			selfApiKey = strings.TrimPrefix(authHeader, authPrefix)
+			c.Request.Header.Set("Authorization", authPrefix+apiKey)
+		}
+	}
+	resp, err := proxy(c, transport)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	defer resp.Body.Close()
+	// 读取响应Body
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	// 计费
+	if selfApiKey != "" {
+		err := increaseSelfApiKeyTokens(respBody, selfApiKey)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
 		}
-		defer resp.Body.Close()
+	}
 
-		for k, v := range resp.Header {
-			c.Header(k, v[0])
-		}
-		c.Status(resp.StatusCode)
-		c.Writer.Write([]byte{})
+	// 设置响应Header
+	for k, v := range resp.Header {
+		c.Header(k, v[0])
+	}
+	// 设置响应状态码
+	c.Status(resp.StatusCode)
+	// 将响应体复制到原始响应中
+	c.Writer.Write(respBody)
+}
+
+func main() {
+	r := gin.Default()
+	transport := getProxyUrl()
+	apiKey := getApiKey()
+
+	r.Any("/*path", func(c *gin.Context) {
+		handler(c, transport, apiKey)
 	})
 
 	r.Run(":8080")
---
+++ tests/main.py
@@ -0,0 +1,15 @@
+import openai
+
+openai.api_key = "sk-xxx"
+openai.api_base = "http://localhost:8080/v1"
+
+# create a completion
+response = openai.Completion.create(
+    model="text-davinci-003",
+    prompt="Say this is a test",
+    temperature=0,
+    max_tokens=7
+)
+
+# print the completion
+print(response)
